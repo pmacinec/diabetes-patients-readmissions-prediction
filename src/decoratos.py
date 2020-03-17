72e7e7d9459256729c60820754e6bd41d8de8533
Peter Mačinec <peter.macinec@gmail.com>
2020-03-17 20:37:45 +0100

    ADD: decorator to measure transformation duration

---
+++ src/decoratos.py
@@ -0,0 +1,25 @@
+import time
+
+
+def transformer_time_calculation_decorator(transformer_name: str):
+    """
+    Decorator for calculation duration of running transformer.
+
+    :param transformer_name: name of pipelines transformer.
+    :return: decorator function.
+    """
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            print(f'{transformer_name} transformation started.')
+
+            result = function(*args, **kwargs)
+
+            end_time = time.time()
+            duration = round(end_time - start_time, 2)
+            print(f'{transformer_name} transformation ended, '
+                  f'took {duration} seconds.')
+
+            return result
+        return wrapper
+    return decorator
