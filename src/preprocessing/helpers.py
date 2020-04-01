4ecc1eae125e91cb52800f35d4cb102e17baf826
Peter Mačinec <peter.macinec@gmail.com>
2020-04-01 21:12:33 +0200

    ADD: transform_label helper function

--- src/preprocessing/helpers.py
+++ src/preprocessing/helpers.py
@@ -12,3 +12,17 @@ def split_dataframe(x, y, test_size=0.2):
     :return: x_train, x_test, y_train, y_test subsets.
     """
     return train_test_split(x, y, test_size=test_size, random_state=42)
+
+
+def transform_label(y):
+    """
+    Transform label column to binary classification.
+
+    Original label column contain three values: `<30`, `>30` and `NO`,
+    so those columns are transformed into just `1` and `0` - if patient
+    was early readmitted or not.
+
+    :param y: dataframe with label attribute (dependent variable).
+    :return: dataframe with transformed label.
+    """
+    return y.apply(lambda x: 1 if x == '<30' else 0)
