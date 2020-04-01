6813e8fc6adb7420b338f2ef248529e4f33779f4
Peter Mačinec <peter.macinec@gmail.com>
2020-04-01 20:53:38 +0200

    CHANGE: refactor split_dataframe helper

--- src/preprocessing/helpers.py
+++ src/preprocessing/helpers.py
@@ -1,15 +1,14 @@
 from sklearn.model_selection import train_test_split
 
 
-def split_dataframe(df, label_col='label', test_size=0.2):
+def split_dataframe(x, y, test_size=0.2):
     """
     Split dataframe into train and test subsets.
 
-    :param df: dataframe to be splitted.
-    :param label_col: name of column with label.
+    :param x: dataframe with attributes for training (independent
+        variables).
+    :param y: dataframe with label attribute (dependent variable).
     :param test_size: size of test subset (default: 0.2).
     :return: x_train, x_test, y_train, y_test subsets.
     """
-    x = df.drop(label_col, axis=1)
-    y = df[label_col]
     return train_test_split(x, y, test_size=test_size, random_state=42)
