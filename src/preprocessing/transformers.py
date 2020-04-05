cee31daf097402c6607435cb985e2acb2f7ee0a3
Peter Mačinec <peter.macinec@gmail.com>
2020-04-05 19:38:54 +0200

    ADD: PandasFeatureUnion transformer

--- src/preprocessing/transformers.py
+++ src/preprocessing/transformers.py
@@ -1,9 +1,62 @@
 import numpy as np
 import pandas as pd
 import string
+from scipy import sparse
 from functools import reduce
+from sklearn.externals.joblib import Parallel, delayed
 from sklearn.base import TransformerMixin
 from src.decoratos import transformer_time_calculation_decorator
+from sklearn.pipeline import FeatureUnion,  _fit_transform_one, _transform_one
+
+
+class PandasFeatureUnion(FeatureUnion):
+    """
+    Feature union transformer for pandas.
+    
+    Reference: https://github.com/marrrcin/pandas-feature-union
+    """
+
+    def fit_transform(self, X, y=None, **fit_params):
+        self._validate_transformers()
+        result = Parallel(n_jobs=self.n_jobs)(
+            delayed(_fit_transform_one)(
+                transformer=trans,
+                X=X,
+                y=y,
+                weight=weight,
+                **fit_params)
+            for name, trans, weight in self._iter())
+
+        if not result:
+            # All transformers are None
+            return np.zeros((X.shape[0], 0))
+        Xs, transformers = zip(*result)
+        self._update_transformer_list(transformers)
+        if any(sparse.issparse(f) for f in Xs):
+            Xs = sparse.hstack(Xs).tocsr()
+        else:
+            Xs = self.merge_dataframes_by_column(Xs)
+        return Xs
+
+    def merge_dataframes_by_column(self, Xs):
+        return pd.concat(Xs, axis="columns", copy=False)
+
+    def transform(self, X):
+        Xs = Parallel(n_jobs=self.n_jobs)(
+            delayed(_transform_one)(
+                transformer=trans,
+                X=X,
+                y=None,
+                weight=weight)
+            for name, trans, weight in self._iter())
+        if not Xs:
+            # All transformers are None
+            return np.zeros((X.shape[0], 0))
+        if any(sparse.issparse(f) for f in Xs):
+            Xs = sparse.hstack(Xs).tocsr()
+        else:
+            Xs = self.merge_dataframes_by_column(Xs)
+        return Xs
 
 
 class ColumnsFilter(TransformerMixin):
